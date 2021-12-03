fe4c3dcbd82f5c6f987ba5a22527e5964b8e0cb9
sofia <2122shb00002@iacademy.edu.ph>
2021-12-04 03:24:04 +0800

    finished top div

--- public/main.js
+++ public/main.js
@@ -9,6 +9,9 @@ function getConfirmation() {
     }
 }
 
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (("0"+dt.getDate()).slice(-2)) +"/"+ (dt.getFullYear()) +" "
++ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
 
 const modal = document.querySelector("#modal");
 const modalOverlay = document.querySelector("#modal-overlay");
